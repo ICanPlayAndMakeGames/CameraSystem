d1fe48be25d7a8e3f8697908d199ac8b7c5fb480
ICanPlayAndMakeGames <119879085+ICanPlayAndMakeGames@users.noreply.github.com>
2023-11-10 17:24:56 +0000

    Update This.lua

--- Code/This.lua
+++ Code/This.lua
@@ -1,69 +1,106 @@
-local ds = game:GetService("DataStoreService")
---local newds = ds:GetDataStore("Bob's_Data_Store")
-local newds = ds:GetDataStore("Bob's_Data_Store")
-local badge = game:GetService("BadgeService")
+--Server side code for making a camera like system based of parts
 
+
+-- Import necessary services and modules
+local Rs = game:GetService('RunService') -- Gets the Roblox service RunService
+local Connections
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Module3d = require(ReplicatedStorage:WaitForChild('Module3D')) -- Requires 3rd party module for ease of use
+
+local Players = game.Players:GetPlayers()
+
+-- Function to perform an action with chosen objects and frames
+local function Do(chosen, number, player, Chosens, Frames)
+	local chosen = game.Workspace[chosen.Name]
+
+	table.insert(Chosens, chosen.Name)
+
+	local Frame = player.PlayerGui["Image" .. number]
+	table.insert(Frames, Frame)
+
+	-- Clone and set up the 3D model for display
+	chosen.Archivable = true
+	local NewChosen = chosen:Clone()
+	chosen.Archivable = false
+	NewChosen.Parent = nil
+
+	local Model3D = Module3d:Attach3D(Frame, NewChosen)
+	Model3D:SetDepthMultiplier(-1)
+	Model3D.Camera.FieldOfView = 60
+	Model3D.Visible = true
+
+	-- Configure the camera for the 3D model
+	Frame.ViewportFrame.Camera.CameraType = Enum.CameraType.Scriptable
+	Frame.ViewportFrame.Camera.CFrame = game.Workspace.Cameras["CameraP" .. number].CFrame
+	Frame.ViewportFrame.Camera.Focus = CFrame.new(Vector3.new(0, 0, 0))
+	Frame.ViewportFrame.Size = UDim2.new(1, 0, 1, 0)
+
+	return chosen, Frame
+end
+
+-- Function to view character chosen objects and frames for a player
+local function Extra(repeatt, chosen)
+	for _, player in pairs(game.Players:GetPlayers()) do
+		local Chosens1 = {}
+		local Frames1 = {}
+
+		if repeatt == false then
+			for _, v in pairs(player.PlayerGui:GetChildren()) do
+				if v:IsA('SurfaceGui') then
+					local Chosens = {}
+					local Frames = {}
+					local Number = string.sub(v.Name, 6, #v.Name)
+					Do(chosen, Number, player, Chosens, Frames)
+					task.wait(0.1)
+				end
+			end
+		else
+			for _, v in pairs(player.PlayerGui:GetChildren()) do
+				if v:IsA('SurfaceGui') then
+					local Frame = v
+					local chosen = game.Workspace[chosen.Name]
+					chosen.Archivable = true
+					local NewChosen = chosen:Clone()
+					chosen.Archivable = false
+					Frame.ViewportFrame[chosen.Name]:Destroy()
+					NewChosen.Parent = Frame.ViewportFrame
+					NewChosen:PivotTo(chosen.PrimaryPart.CFrame)
+					task.wait(0.01)
+				end
+			end
+		end
+		task.wait(0.05)
+	end
+end
+
+local Stop = false
+
+-- Function to continuously fire a remote event to clients
+local function AddLoop(char)
+	print("Connection Added")
+	while task.wait(0.1) do
+		game.ReplicatedStorage.Remotes.Chosen:FireAllClients(true, char)
+		if Stop == true then
+			print("Loop broken")
+			break
+		end
+	end
+	print("Connection Broken")
+	return
+end
+
+local Connection
+
+-- Player joining event
 game.Players.PlayerAdded:Connect(function(player)
-	local scoreholder = Instance.new("NumberValue")
-	local why,why2
-	local success,errormss = pcall(function()
-	why = newds:GetAsync("highscore"..player.UserId)
-	why2 = newds:GetAsync("highwave"..player.UserId)
-	end)	
-	local highscore = Instance.new("NumberValue")
-	local highwave = Instance.new("NumberValue")
-	highwave.Parent = player
-	highwave.Name = "HighWave"
-	highscore.Parent = player
-	highscore.Name = "HighScore"
-	if why then
-		highscore.Value = why
-	elseif not why then
-		highscore.Value = 0
-	end
-	
-	if why2 then
-		highwave.Value = why2
-	elseif not highwave then
-		highwave.Value = 0
-	end
-	
-	
-	scoreholder.Name = "score"
-	scoreholder.Parent = player
+	repeat task.wait(0.01) until player.Character
+	game.ServerStorage.Chosen.Value = player.Name
 end)
 
-
-game.ReplicatedStorage.Score.OnServerInvoke = (function(player,score)
-	player.score.Value = score
-	if score > player.HighScore.Value then
-		print("higherScore!!")
-		print(score)
-		player.HighScore.Value = score
-		return true
-	else
-		return false
-	end
-end)
-
-
-game.ReplicatedStorage.WaveChanged.OnServerEvent:Connect(function(player,wave)
-	if wave > player.HighWave.Value then
-		print("Highest Wave")
-		player.HighWave.Value = wave
-		
-	end
-end)
-
-
+-- Player leaving event
 game.Players.PlayerRemoving:Connect(function(player)
-	print(player:WaitForChild("HighScore").Value)
-	--local success,errormsg = pcall(function()
-	--game:BindToClose(function()
-		--local success,errormsg = pcall(function()
-		newds:SetAsync("highscore"..player.UserId,player.HighScore.Value)
-			newds:SetAsync("highwave"..player.UserId,player.HighWave.Value)
-			--end)	
-		--end)
-	--end)
+	if player.Name == game.ServerStorage.Chosen.Value then
+		game.ServerStorage.Chosen.Value = ""
+	end
 end)
